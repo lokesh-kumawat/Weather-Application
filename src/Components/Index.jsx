378189d527d45a00af5eeb00ff78389d403b028f
lokesh-kumawat <lokeshkumawat5590@gmail.com>
2024-11-10 05:54:30 -0800

    first commit

---
+++ src/Components/Index.jsx
@@ -0,0 +1,11 @@
+// export {default as BackgroundLayout} from './BackgroundLayout'
+// export {default as MiniCard} from './MiniCard'
+// export {default as WeatherCard} from './WeatherCard'
+
+import BackgroundLayout from './BackgroundLayout'
+import MiniCard from './MiniCard'
+import WeatherCard from './WeatherCard'
+
+export { BackgroundLayout}
+export { MiniCard }
+export { WeatherCard}
